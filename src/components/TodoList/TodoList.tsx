64c70f1cd537839d3c51ae2550bb029cd14aa7fe
Robert Siński <robert.sinski@outlook.com>
2024-04-19 14:36:56 +0200

    feat: redux-persist init and config and null state for todo list

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -8,6 +8,19 @@ import TodoItems from "./Partials/TodoItems";
 const TodoList: React.FC = (): JSX.Element => {
   const todos = useAppSelector((state) => state.global.todos);
   const completedTodos = todos.filter((todo) => todo.isCompleted);
+
+  if (!todos.length) {
+    return (
+      <div className="w-full max-w-xl flex flex-col items-start justify-start">
+        <h1 className="text-gray-300 text-xl">No todos found</h1>
+        <p className="text-muted-foreground">
+          You can add a todo by typing in the input field and pressing the enter
+          key.
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div className="w-full max-w-xl flex flex-col items-start justify-start">
       <div className="flex items-center gap-2">
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,24 @@
 import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
 import { isLocal } from "@/constatnt/env";
 import globalReducer from "@/state/globalSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, globalReducer);
 
 export const store = configureStore({
   reducer: {
-    global: globalReducer,
+    global: persistedReducer,
   },
   devTools: isLocal,
 });
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
